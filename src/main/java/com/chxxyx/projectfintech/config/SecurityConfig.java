3e3b01a38603fc4fa3ba4547171715bb017c4d21
chxxyx <codud0826@naver.com>
2022-12-01 22:42:08 +0900

    chore : security 설정

--- src/main/java/com/chxxyx/projectfintech/config/SecurityConfig.java
+++ src/main/java/com/chxxyx/projectfintech/config/SecurityConfig.java
@@ -1,2 +1,30 @@
-package com.chxxyx.projectfintech.config;public class SecurityConfig {
+package com.chxxyx.projectfintech.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+
+		// (rest api) 외부에서의 호출을 위한
+		http
+			//.cors().disable()
+			.csrf().disable(); // csrf 토큰 비활성화
+		http    .headers().frameOptions().sameOrigin();
+
+		http
+			.authorizeRequests()
+			.anyRequest().permitAll();
+
+		return http.build();
+	}
+
 }
--- src/main/java/com/chxxyx/projectfintech/main/MainController.java
+++ src/main/java/com/chxxyx/projectfintech/main/MainController.java
@@ -1,2 +1,15 @@
-package com.chxxyx.projectfintech.main;public class MainController {
+package com.chxxyx.projectfintech.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String index() {
+
+		return "index";
+	}
+
 }
