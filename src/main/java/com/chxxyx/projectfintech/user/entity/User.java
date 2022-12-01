07058e4f260ce1c96f16df8257f9c040f2c21756
chxxyx <codud0826@naver.com>
2022-12-01 22:56:35 +0900

    feat : User Entity 생성

---
+++ src/main/java/com/chxxyx/projectfintech/user/entity/User.java
@@ -0,0 +1,40 @@
+package com.chxxyx.projectfintech.user.entity;
+
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "BINARY(16)")
+	private UUID id;// 식별 값
+
+	private String SSN; //주민번호
+
+	private String userName; //유저 이름
+	private String password; //비밀번호
+	private LocalDateTime createdAt; //회원 생성일
+	private LocalDateTime modifiedAt; //회원 정보 수정일
+
+	@Column(unique =true)
+	private String emailId; //유저 id
+	private Boolean emailAuthYn; //이메일 인증 여부
+	private String userType; // 관리자, 회원 구분
+
+}
