8f8d2e16e48618ef667ff02cbf8fd6f9acff0325
chxxyx <codud0826@naver.com>
2022-12-02 16:11:32 +0900

    feat :  Validator 수정 (UserDto)

--- src/main/java/com/chxxyx/projectfintech/user/dto/UserDto.java
+++ src/main/java/com/chxxyx/projectfintech/user/dto/UserDto.java
@@ -2,6 +2,7 @@ package com.chxxyx.projectfintech.user.dto;
 
 import java.time.LocalDateTime;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,20 +14,20 @@ import lombok.NoArgsConstructor;
 @Data
 public class UserDto {
 
-	@NotEmpty(message = "사용자 주민 번호는 필수 항목입니다.")
+	@NotBlank(message = "사용자 주민 번호는 필수 항목입니다.")
 	private String SSN;//주민번호
 
-	@NotEmpty(message = "사용자 이름은 필수 항목입니다.")
+	@NotBlank(message = "사용자 이름은 필수 항목입니다.")
 	private String userName;//유저 이름
 
-	@NotEmpty(message = "사용자 비밀번호는 필수 항목입니다.")
+	@NotBlank(message = "사용자 비밀번호는 필수 항목입니다.")
 	private String password;
 
 	private LocalDateTime createdAt;
 
 	private LocalDateTime modifiedAt;
 
-	@NotEmpty(message = "사용자 ID(email)는 필수 항목입니다.")
+	@NotBlank(message = "사용자 ID(email)는 필수 항목입니다.")
 	@Email
 	private String emailId; //유저 id
 	private Boolean emailAuthYn; //이메일 인증 여부
