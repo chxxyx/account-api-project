38ad13c0d68e65e1b1bf3745b0c6505b51893481
chxxyx <codud0826@naver.com>
2022-12-24 18:59:07 +0900

    feat : (관리자) 회원, 계좌 정보 조회 Pagination 처리 추가

--- src/main/java/com/chxxyx/projectfintech/account/repository/AccountRepository.java
+++ src/main/java/com/chxxyx/projectfintech/account/repository/AccountRepository.java
@@ -5,14 +5,20 @@ import com.chxxyx.projectfintech.user.entity.User;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository <Account, String> {
+public interface AccountRepository extends JpaRepository<Account, String> {
+
 	Optional<Account> findFirstByOrderByAccountNumberDesc();
+
 	Integer countByAccountUser(User accountUser);
 
 	Optional<Account> findByAccountNumber(String accountNumber);
 
 	List<Account> findByAccountUser(User accountUser);
+
+	Page<Account> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
 }
--- src/main/java/com/chxxyx/projectfintech/admin/service/AdminService.java
+++ src/main/java/com/chxxyx/projectfintech/admin/service/AdminService.java
@@ -9,6 +9,9 @@ import com.chxxyx.projectfintech.user.repository.UserRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,14 +23,15 @@ public class AdminService {
 
 	public List<UserDto> getUserInfo() {
 
-		List<User> user = userRepository.findAll();
+		Pageable limit = PageRequest.of(0, 10);
+		Page<User> user = userRepository.findAllByOrderByCreatedAtDesc(limit);
 		return user.stream().map(UserDto::fromEntity).collect(Collectors.toList());
 
 	}
 
 	public List<AccountDto> getUserAccountInfo() {
-		List<Account> accounts = accountRepository.findAll();
-
+		Pageable limit = PageRequest.of(0, 10);
+		Page<Account> accounts = accountRepository.findAllByOrderByCreatedAtDesc(limit);
 		return accounts.stream().map(AccountDto::fromEntity).collect(Collectors.toList());
 	}
 
