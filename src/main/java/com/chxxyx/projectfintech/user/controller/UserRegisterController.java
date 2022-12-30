bd60a5c1b7adba27f52d0623279d040c31284ee9
chxxyx <codud0826@naver.com>
2022-12-31 00:17:45 +0900

    feat : 회원가입 기능 수정
    Rest Controller 사용 변경

--- src/main/java/com/chxxyx/projectfintech/user/controller/UserRegisterController.java
+++ src/main/java/com/chxxyx/projectfintech/user/controller/UserRegisterController.java
@@ -1,38 +1,39 @@
 package com.chxxyx.projectfintech.user.controller;
 
+import com.chxxyx.projectfintech.user.dto.RegisterUser;
 import com.chxxyx.projectfintech.user.dto.UserDto;
 import com.chxxyx.projectfintech.user.service.UserService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RestController
 @RequiredArgsConstructor
-@Controller
 public class UserRegisterController {
 
 	private final UserService userService;
 
-	@GetMapping("/user/register")
-	public String userRegister(UserDto userDto) {
-
-		return "user/register";
-	}
-
 	@PostMapping("/user/register")
-	public String userRegisterSubmit(
-		@Valid UserDto parameter, BindingResult bindingResult, Model model) {
-		model.addAttribute("userDto", parameter);
+	public RegisterUser.Response userRegister(@RequestBody RegisterUser.Request request) {
 
-		if (bindingResult.hasErrors()) {
-			return "user/register";
-		}
+		log.info("::::::::::" + request.getSsn());
+		log.info("::::::::::" + request.getUsername());
+		log.info("::::::::::" + request.getPassword());
+		log.info("::::::::::" + request);
 
-		userService.userRegister(parameter);
+		return RegisterUser.Response.from(
+			userService.registerUser(request.getUsername(), request.getPassword(),
+				request.getName(), request.getSsn()));
 
-		return "redirect:/";
 	}
+
 }
