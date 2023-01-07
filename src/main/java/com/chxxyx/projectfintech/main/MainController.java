0f91c74728b895959644a79d8a4af16e316ab656
chxxyx <codud0826@naver.com>
2023-01-07 17:51:52 +0900

    style : 기타 코드 삭제

--- src/main/java/com/chxxyx/projectfintech/main/MainController.java
+++ src/main/java/com/chxxyx/projectfintech/main/MainController.java
@@ -6,10 +6,4 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-	@RequestMapping("/")
-	public String index() {
-
-		return "index";
-	}
-
 }
